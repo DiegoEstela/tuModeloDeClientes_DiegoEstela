304d92c1d7db7f9e975e7411bc88aea9b757a657
DiegoEstela <die.estela@gmail.com>
2023-09-12 14:31:57 +0200

    add indent

--- packages/manejo_clientes.py
+++ packages/manejo_clientes.py
@@ -5,7 +5,7 @@ from packages.cliente import Cliente
 def guardar_clientes(clientes, archivo):
     with open(archivo, 'w') as f:
         data = [cliente.__dict__ for cliente in clientes]
-        json.dump(data, f)
+        json.dump(data, f, indent=4)
 
 
 def cargar_clientes(archivo):
